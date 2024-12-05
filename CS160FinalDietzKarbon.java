4802c1e50fbd669e6effc25df2029bda6d4c7740
BalakeKarbon <blakekarbon@gmail.com>
2024-12-05 20:06:19 +0000

    Got it all reading properly!

--- CS160FinalDietzKarbon.java
+++ CS160FinalDietzKarbon.java
@@ -11,28 +11,39 @@ import java.awt.image.DataBufferByte;
 
 public class CS160FinalDietzKarbon {
 	public static void main(String args[]) {
+		/* Notes:
+		 * Ask the user what file they want to store data in
+		 * Ask the user how many bits they want to use in that storage
+		 * Tell the user how much data that will allow to store in the image
+		 * Make sure the user is okay with that information
+		 *
+		 */
 		String path = "test.png";
 		try {
 			BufferedImage containerImg = ImageIO.read(new File(path));
-			//Testing... Will all be re wrote!
 			int bitsPerColor = 2;//Must be less than 8 if your doing 24 bit color and storing for even/odd!!!! higher the number more distortion!!!!
-			System.out.println(containerImg.getColorModel().toString());
 			boolean hasAlpha = (containerImg.getAlphaRaster() != null);
 			int pixelByteCount = 3;
 			if(hasAlpha) {
 				pixelByteCount = 4;
 			}
-			int containerImgWidth = containerImg.getWidth();
-			int containerImgHeight = containerImg.getHeight();
-			/*for(int x = 0;x<(containerImgWidth);x++) {
-				for(int y = 0;y<(containerImgHeight);y++) {
+			//int containerImgWidth = containerImg.getWidth();
+			//int containerImgHeight = containerImg.getHeight();
+			//System.out.println(pixelByteCount);
+			for(int x = 0;x<(containerImg.getWidth());x++) {
+				for(int y = 0;y<(containerImg.getHeight());y++) {
 					System.out.print(" ");
 					for(int c = 0;c<pixelByteCount;c++) {
-						System.out.print(Integer.toHexString(pixels[x*y])+",");
+						int colorInt = containerImg.getRGB(x,y);
+						int[] pixelColor = new int[pixelByteCount];
+						for(int i = 0;i<pixelByteCount;i++) {
+							pixelColor[i]=(colorInt & (0xff << (i*8))) >> (i * 8);
+						}
+						System.out.print(pixelColor[0] + ",");
 					}
 				}
 				System.out.println("");
-			}*/
+			}
 			
 		} catch (IOException e) {
 			System.out.println("test");
