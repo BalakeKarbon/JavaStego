9b11ea2edafb885b55c43b4e1969909a86433f84
BalakeKarbon <blakekarbon@gmail.com>
2024-12-06 05:35:56 +0000

    Model seems funtional...

--- CS160FinalDietzKarbonWriter.java
+++ CS160FinalDietzKarbonWriter.java
@@ -3,6 +3,7 @@ import java.io.File;
 import java.io.IOException;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
+import java.nio.charset.Charset;
 
 public class CS160FinalDietzKarbonWriter {
 	public static BufferedImage getContainerImage(Scanner scnr) {
@@ -22,10 +23,91 @@ public class CS160FinalDietzKarbonWriter {
 		}
 		return containerImg;
 	}
+	public static int getBitsPerByte(Scanner scnr) {
+		int bitsPerByte = 0;
+		boolean foundBits = false;
+		while(!foundBits) {
+			System.out.print("Bits:");
+			bitsPerByte = scnr.nextInt();
+			if(!(bitsPerByte <= 8 && bitsPerByte > 0)) {
+				System.out.println("Only 8 bits per byte! Encoded bits must be value between 1 and 8 inclusive.");
+			} else {
+				foundBits = true;
+			}
+		}
+		return bitsPerByte;
+	}
+	public static byte[] getSecretData(Scanner scnr,long bytesOfStorage) {
+		// TO-DO: Add the ability to specify a end of message modifier for recognition by the reader.
+		boolean dataFits = false;
+		byte[] secretData = new byte[0];
+		while(!dataFits) {
+			System.out.print("Please enter data to encode:");
+			secretData = scnr.nextLine().getBytes(Charset.forName("UTF-8"));
+			if(secretData.length<=bytesOfStorage) {
+				dataFits = true;
+			} else {
+				System.out.println("Data does not fit withing the available storage!");
+			}
+		}
+		return secretData;
+	}
 	public static void main(String args[]) {
 		Scanner scnr = new Scanner(System.in);
 		System.out.println("What PNG would you like to store your secred data in?");
 		BufferedImage containerImg = getContainerImage(scnr);
-
+		System.out.println("How many bits per byte would you like to encode?");
+		int bitsPerByte = getBitsPerByte(scnr);
+		int bytesPerPixel = (containerImg.getAlphaRaster() != null) ? 4 : 3;
+		long bytesOfStorage = (containerImg.getWidth() * containerImg.getHeight() * bytesPerPixel * bitsPerByte)/8; // Possibly add printing how much storage is available.
+		scnr.nextLine(); // Mention that this is due to weird scanner behavior.
+		byte[] secretData = getSecretData(scnr, bytesOfStorage);
+		//Encode data
+		int currentPixelColor, newPixelColor;
+		int pixelColorMask = Integer.valueOf((1 << (bytesPerPixel*8)) - 1).byteValue();
+		boolean stillData = true;
+		System.out.println(""); // TEMPORARY
+		for(int x = 0;x<containerImg.getWidth() && stillData;x++) {
+			for(int y = 0;y<containerImg.getHeight() && stillData;y++) {
+				int currentPixelOffset = (x*containerImg.getWidth())+y;
+				int containerImgByteOffset = currentPixelOffset*bytesPerPixel;
+				currentPixelColor = containerImg.getRGB(x,y);
+				newPixelColor = currentPixelColor & pixelColorMask;
+				for(int colorComponentIndex = 0;colorComponentIndex<bytesPerPixel && stillData;colorComponentIndex++) {
+					int colorComponentIndexShift = colorComponentIndex * 8;
+					byte colorComponent = Integer.valueOf(currentPixelColor >> (colorComponentIndex*8)).byteValue(); // This gets the color component by shifting the currentPixelColor integer 8*colorComponentIndex bits and then converting to a byte therebye cutting off the top 24 bits.
+					int containerImgByteIndex = (containerImgByteOffset+colorComponentIndex);
+					int secretDataByteIndex = (containerImgByteIndex*bitsPerByte)/8;
+					if(secretDataByteIndex>=secretData.length) {
+						stillData = false;
+						break; // Possibly continue depending on what happens next.
+					}
+					
+					//System.out.print("("+ containerImgByteIndex +","+ secretDataByteIndex +")");
+					//System.out.printf("%02X",colorComponent);
+					//System.out.print(Integer.toBinaryString(colorComponent) + ",");
+					// Now time to encode data
+					// (everything above x bits of color component OR'ed with the x bits?)
+					byte secretDataMask = Integer.valueOf((1 << bitsPerByte) - 1).byteValue();
+					byte colorComponentMask = Integer.valueOf(~secretDataMask).byteValue();
+					byte secretDataSlice = Integer.valueOf((secretData[secretDataByteIndex] >> (colorComponentIndex*bitsPerByte)) & secretDataMask).byteValue();
+					byte colorComponentSlice = Integer.valueOf(colorComponent & colorComponentMask).byteValue();
+					byte newColorComponent = Integer.valueOf(colorComponentSlice | secretDataSlice).byteValue();
+					//System.out.print(Integer.toBinaryString()+"");
+					//System.out.print("("+Integer.toBinaryString(colorComponent)+"-");
+					//System.out.print(Integer.toBinaryString(newColorComponent)+")");
+					// Now we must convert these color components back into a 32 bit integer which will actually be a 24 bit integer....
+					int newColorComponentMask = (0xff << (colorComponentIndexShift));
+					int newPixelColorMask = ~newColorComponentMask;
+					newPixelColor = (newPixelColor & newPixelColorMask) | ((newColorComponent << colorComponentIndexShift) & newColorComponentMask);
+				}
+				System.out.printf("%02X->%02X",currentPixelColor,newPixelColor);
+				//System.out.print(Integer.toBinaryString(currentPixelColor)+"-");
+				//System.out.print(Integer.toBinaryString(newPixelColor));
+				System.out.print(","); // TEMPORARY
+			}
+			System.out.println(""); // TEMPORARY
+		}
+		// THEN write to output file.
 	}
 }
