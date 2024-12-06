a21d58a15307973387998543925d62114b8fb33e
BalakeKarbon <blakekarbon@gmail.com>
2024-12-06 07:07:22 +0000

    Almost finished decoder...

--- CS160FinalDietzKarbonWriter.java
+++ CS160FinalDietzKarbonWriter.java
@@ -69,8 +69,7 @@ public class CS160FinalDietzKarbonWriter {
 			}
 		}
 	}
-	public static void main(String args[]) {
-		Scanner scnr = new Scanner(System.in);
+	public static void stegEncode(Scanner scnr) {
 		System.out.println("What PNG would you like to store your secred data in?");
 		BufferedImage containerImg = getContainerImage(scnr);
 		System.out.println("How many bits per byte would you like to encode?");
@@ -128,6 +127,78 @@ public class CS160FinalDietzKarbonWriter {
 		}
 		// THEN write to output file.
 		System.out.println("What file would you like to save this encoded PNG to?");
-		writeEncodedImage(scnr, containerImg);	
+		writeEncodedImage(scnr, containerImg);
+	}
+	public static void stegDecode(Scanner scnr) {
+		System.out.println("What PNG would you like to decode?");
+		BufferedImage containerImg = getContainerImage(scnr);
+		System.out.println("How many bits per byte were used during encodeing?");
+		int bitsPerByte = getBitsPerByte(scnr);
+		int bytesPerPixel = (containerImg.getAlphaRaster() != null) ? 4 : 3;
+		int bytesOfStorage = (containerImg.getWidth() * containerImg.getHeight() * bytesPerPixel * bitsPerByte)/8; // Possibly add printing how much storage is available.
+		byte[] secretData = new byte[bytesOfStorage];
+		//Decode data
+		int currentPixelColor;
+		int secretDataShift = 0;
+		for(int x = 0;x<containerImg.getWidth();x++) {
+			for(int y = 0;y<containerImg.getHeight();y++) {
+				int currentPixelOffset = (x*containerImg.getWidth())+y;
+				int containerImgByteOffset = currentPixelOffset*bytesPerPixel;
+				currentPixelColor = containerImg.getRGB(x,y);
+				for(int colorComponentIndex = 0;colorComponentIndex<bytesPerPixel;colorComponentIndex++) {
+					int colorComponentIndexShift = colorComponentIndex * 8;
+					byte colorComponent = Integer.valueOf(currentPixelColor >> (colorComponentIndex*8)).byteValue(); // This gets the color component by shifting the currentPixelColor integer 8*colorComponentIndex bits and then converting to a byte therebye cutting off the top 24 bits.
+					int containerImgByteIndex = (containerImgByteOffset+colorComponentIndex);
+					int secretDataByteIndex = (containerImgByteIndex*bitsPerByte)/8;
+					byte secretDataMask = Integer.valueOf((1<<bitsPerByte)-1).byteValue();
+					int containerImgIndex = containerImgByteOffset+colorComponentIndex;
+					System.out.print(Integer.toBinaryString(secretData[secretDataByteIndex]) + ":");
+					secretData[secretDataByteIndex] = Integer.valueOf(secretData[secretDataByteIndex] | ((colorComponent & secretDataMask)<<secretDataShift)).byteValue();
+					secretDataShift+=bitsPerByte;
+					if(secretDataShift>7) {
+						secretDataShift = 0;
+						System.out.print((char)secretData[secretDataByteIndex]);
+					}
+					//System.out.print("("+ containerImgByteIndex +","+ secretDataByteIndex +")");
+					//System.out.printf("%02X",colorComponent);
+					//System.out.print(Integer.toBinaryString(colorComponent) + ",");
+					// Now time to encode data
+					// (everything above x bits of color component OR'ed with the x bits?)
+					/*byte secretDataMask = Integer.valueOf((1 << bitsPerByte) - 1).byteValue();
+					byte colorComponentMask = Integer.valueOf(~secretDataMask).byteValue();
+					byte secretDataSlice = Integer.valueOf((secretData[secretDataByteIndex] >> (colorComponentIndex*bitsPerByte)) & secretDataMask).byteValue();
+					byte colorComponentSlice = Integer.valueOf(colorComponent & colorComponentMask).byteValue();
+					byte newColorComponent = Integer.valueOf(colorComponentSlice | secretDataSlice).byteValue();*/
+					//System.out.print(Integer.toBinaryString()+"");
+					//System.out.print("("+Integer.toBinaryString(colorComponent)+"-");
+					//System.out.print(Integer.toBinaryString(newColorComponent)+")");
+					// Now we must convert these color components back into a 32 bit integer which will actually be a 24 bit integer....
+					/*int newColorComponentMask = (0xff << (colorComponentIndexShift));
+					int newPixelColorMask = ~newColorComponentMask;*/
+				}
+				//System.out.printf("%02X->%02X",currentPixelColor,newPixelColor);
+				//System.out.print(Integer.toBinaryString(currentPixelColor)+"-");
+				//System.out.print(Integer.toBinaryString(newPixelColor));
+				//System.out.print(","); // TEMPORARY
+			}
+			//System.out.println(""); // TEMPORARY
+		}
+		// THEN write to output file.
+	}
+	public static void main(String args[]) {
+		Scanner scnr = new Scanner(System.in);
+		System.out.println("Would you like to encode or decode a PNG?");
+		char choice = '!';
+		while(choice == '!') {
+			System.out.print("Enter \"e\" for encode or \"d\" for decode:");
+			choice = scnr.nextLine().toLowerCase().charAt(0);
+			if(choice == 'e') {
+				stegEncode(scnr);
+			} else if(choice == 'd') {
+				stegDecode(scnr);
+			} else {
+				choice = '!';
+			}
+		}
 	}
 }
