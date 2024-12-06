fb3de47ff6bff4578a1b310569e1f5e7d9746018
BalakeKarbon <blakekarbon@gmail.com>
2024-12-06 02:53:47 +0000

    Something is only slightly off...

--- CS160FinalDietzKarbon.java
+++ CS160FinalDietzKarbon.java
@@ -22,37 +22,49 @@ public class CS160FinalDietzKarbon {
 		String path = "test.png";
 		try {
 			BufferedImage containerImg = ImageIO.read(new File(path));
-			int bitsPerColor = 1;//Must be less than 8 if your doing 24 bit color and storing for even/odd!!!! higher the number more distortion!!!!
+			int bitsPerColor = 2;//Must be less than 8 if your doing 24 bit color and storing for even/odd!!!! higher the number more distortion!!!!
 			boolean hasAlpha = (containerImg.getAlphaRaster() != null);
 			int pixelByteCount = 3;
 			if(hasAlpha) {
 				pixelByteCount = 4;
 			}
 			int bytesOfStorage = (containerImg.getWidth() * containerImg.getHeight() * pixelByteCount * bitsPerColor)/8; //Will have issues as not long? Test with large image!?
-			byte[] secretData = "Test".getBytes(Charset.forName("UTF-8"));
+			byte[] secretData = "TTTT".getBytes(Charset.forName("UTF-8"));
 			if(bytesOfStorage >= secretData.length) {
 			//int containerImgWidth = containerImg.getWidth();
 			//int containerImgHeight = containerImg.getHeight();
 			//System.out.println(pixelByteCount);
-				int secretIndex = 0;
+				int secretByteIndex = 0;
+				int secretBitIndex = 0;
 				for(int x = 0;x<(containerImg.getWidth());x++) {
 					for(int y = 0;y<(containerImg.getHeight());y++) {
-						System.out.print(" ");
-						int colorInt = containerImg.getRGB(x,y);
-						int[] pixelColor = new int[pixelByteCount];
-						for(int c = 0;c<pixelByteCount;c++) {
-							secretIndex=(((x*y)+y)+c)/bitsPerColor; //THIS IS BAD
-							pixelColor[c]=(colorInt & (0xff << (c*8))) >> (c * 8);
-							System.out.printf("(%d %02X)",secretIndex,pixelColor[c]);
-							//Modify here and re write to new BufferedImage in order to save for later!
+						if(secretByteIndex<secretData.length) {
+							int colorInt = containerImg.getRGB(x,y);
+							byte[] pixelColor = new byte[pixelByteCount];
+							int newColorInt = colorInt;
+							for(int c = 0;c<pixelByteCount;c++) {
+								secretByteIndex=((((((x*containerImg.getWidth())+y)*pixelByteCount))+c)*bitsPerColor)/8;
+								if(secretByteIndex<secretData.length) {
+									newColorInt = colorInt & (~((1 << (8*pixelByteCount))-1));
+									pixelColor[c]=(Integer.valueOf((colorInt & (0xff << (c*8))) >> (c * 8))).byteValue();
+									byte newColor = Integer.valueOf(pixelColor[c] & ((~((1 << bitsPerColor)-1)) | ((secretData[secretByteIndex] >> secretBitIndex) & ((1 << bitsPerColor) - 1)))).byteValue();
+									secretBitIndex+=bitsPerColor;
+									if(secretBitIndex>7) {
+										secretBitIndex = 0;
+									}
+									newColorInt = newColorInt | (((int)newColor) << (c*8));
+								}
+							}
+							System.out.println("("+Integer.toBinaryString(colorInt)+":"+Integer.toBinaryString(newColorInt)+")");
+							containerImg.setRGB(x,y,newColorInt);
 						}
 					}
-					System.out.println("");
 				}
+				File outputFile = new File("out.png");
+				ImageIO.write(containerImg, "png", outputFile);
 			} else {
 				System.out.println("Here we have to go back through loop and let homie know it wont fit.");
 			}
-			
 		} catch (IOException e) {
 			System.out.println("test");
 		}
